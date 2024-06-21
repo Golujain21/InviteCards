7772b0fa72f7a5f058a52f8273f1d61cf592a3ea
Golujain21 <119733472+Golujain21@users.noreply.github.com>
2024-06-21 10:46:02 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -44,6 +44,27 @@ export default function Home() {
             enjoyable. Join us in bringing smiles and spreading joy, one
             personalized card at a time!
           </h2>
+<h3
+            style={{
+              fontSize: "18px",
+              fontFamily: "Poppins",
+              margin: "0px",
+              maxWidth: "1300px",
+              textAlign: "center",
+            }}
+          >
+            Here's a paragraph you can use for a cards creating website: Welcome
+            to our cards creating website, where creativity meets customization!
+            Whether you're celebrating a special occasion, sending heartfelt
+            wishes, or simply expressing your creativity, our platform offers
+            you the tools to design the perfect card. Choose from a variety of
+            templates, personalize with your own messages and images, and tailor
+            every detail to suit your unique style. Whether it's a birthday,
+            anniversary, holiday greeting, or a heartfelt thank-you note, our
+            intuitive interface makes crafting meaningful cards effortless and
+            enjoyable. Join us in bringing smiles and spreading joy, one
+            personalized card at a time!
+          </h3>
         </div>
       </MainLayout>
     </>
