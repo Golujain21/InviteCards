26cae443d5d351e6c75423dcd8ae5cb53f10e93c
Golujain21 <119733472+Golujain21@users.noreply.github.com>
2024-06-21 11:43:09 +0530

    Update page.jsx
    add h1 tag

--- app/cards/[language]/[category]/[sub-category]/page.jsx
+++ app/cards/[language]/[category]/[sub-category]/page.jsx
@@ -2,13 +2,13 @@ import MainLayout from "@/app/_components/MainLayout";
 import { metadata } from "@/app/layout";
 import React from "react";
 
-const SubCategory = () => {
+const SubCategory = ({params}) => {
 
-    metadata.title="Sub-category Page",
-metadata.description="description from sub-category page"
+    metadata.title=`Sub-category Page ${params.language} ${params.category} ${params.sub-category}`,
+metadata.description=`description from sub-category page  ${params.language} ${params.category} ${params.sub-category}`
   return (
     <MainLayout>
-      <div>SubCategory</div>
+      <h1>SubCategory</h1>
     </MainLayout>
   );
 };
