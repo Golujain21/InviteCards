297ce61dcba6a0ec5fcd1e672e92569ee5cb61a3
Golujain21 <119733472+Golujain21@users.noreply.github.com>
2024-06-21 10:57:07 +0530

    Update Language.jsx
    initial changes

--- app/_components/Language.jsx
+++ app/_components/Language.jsx
@@ -1,45 +1,32 @@
-'use client'
-import React, { useEffect, useState } from 'react'
 import Link from "next/link";
-import { LanguageData } from '../cards/_utils/utils';
-const LanguageClient = ({params}) => {
 
-    const [currLanguage, setCurrLanguage] = useState(null);
-
-    useEffect(() => {
-      setCurrLanguage(
-        ...LanguageData.filter((item) => item.language == params.language)
-      );
-    }, [params.language]);
-
+const LanguageClient = ({ currLanguage,params }) => {
   return (
-    <div
-    className="container"
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      gap: "10px",
-      padding:"50px 0px"
-    }}
-  >
-    <h1 style={{fontSize:'40px',fontFamily:"inherit",textAlign:"center"}}>{currLanguage?.shadi}</h1>
-    <h2 style={{fontSize:'20px',fontFamily:"inherit",textAlign:"center"}}>{currLanguage?.description}</h2>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        gap: "10px",
-        width: "100%",
-        marginTop: "40px",
-      }}
-    >
-      <div className="container">
-        <div className="row">
-          {currLanguage?.category.map((item,i) => (
-           
+    <>
+      <h1
+        style={{ fontSize: "40px", fontFamily: "inherit", textAlign: "center" }}
+      >
+        {currLanguage.shadi}
+      </h1>
+      <h2
+        style={{ fontSize: "20px", fontFamily: "inherit", textAlign: "center" }}
+      >
+        {currLanguage.description}
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "10px",
+          width: "100%",
+          marginTop: "40px",
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            {currLanguage.category.map((item, i) => (
               <div
-              key={i}
+                key={i}
                 className="col-sm-3"
                 style={{
                   display: "flex",
@@ -57,11 +44,10 @@ const LanguageClient = ({params}) => {
                   style={{ width: "100%" }}
                 />
                 <Link
-                  href={`${params.language}/${item.replace(" ", "")}`}
+                  href={`/cards/${params.language}/${item.replace(" ", "")}`}
                   style={{
                     padding: "20px",
-                    boxShadow:
-                      "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
                     borderRadius: "10px",
                     cursor: "pointer",
                   }}
@@ -69,14 +55,12 @@ const LanguageClient = ({params}) => {
                   {item}
                 </Link>
               </div>
-          
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+    </>
+  );
+};
 
-  )
-}
-
-export default LanguageClient
+export default LanguageClient;
