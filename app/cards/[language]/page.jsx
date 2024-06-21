334baa477e22c7d3f6d9bccf4dc10b793ad0c24b
Golujain21 <119733472+Golujain21@users.noreply.github.com>
2024-06-21 10:56:18 +0530

    Update page.jsx
    initial changes

--- app/cards/[language]/page.jsx
+++ app/cards/[language]/page.jsx
@@ -1,8 +1,7 @@
 import MainLayout from "@/app/_components/MainLayout";
 import { metadata } from "@/app/layout";
 import { LanguageData } from "../_utils/utils";
-import Link from "next/link";
-// import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
+import LanguageClient from "@/app/_components/Language";
 
 const Language = ({ params }) => {
   // Filter LanguageData based on params.language
@@ -19,39 +18,7 @@ const Language = ({ params }) => {
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%", minHeight: "800px" }}>
         <div className="container" style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px", padding: "50px 0px" }}>
           {currLanguage && (
-            <>
-              <h1 style={{ fontSize: "40px", fontFamily: "inherit", textAlign: "center" }}>
-                {currLanguage.shadi}
-              </h1>
-              <h2 style={{ fontSize: "20px", fontFamily: "inherit", textAlign: "center" }}>
-                {currLanguage.description}
-              </h2>
-              <div style={{ display: "flex", justifyContent: "space-between", gap: "10px", width: "100%", marginTop: "40px" }}>
-                <div className="container">
-                  <div className="row">
-                    {currLanguage.category.map((item, i) => (
-                      <div key={i} className="col-sm-3" style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-                        <img
-                          title={item}
-                          loading="lazy"
-                          width="216"
-                          height="300"
-                          src="https://invitecrafter.com/public/uploads/1691480709.jpg"
-                          alt=""
-                          style={{ width: "100%" }}
-                        />
-                        <Link
-                          href={`/cards/${params.language}/${item.replace(" ", "")}`}
-                          style={{ padding: "20px", boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", borderRadius: "10px", cursor: "pointer" }}
-                        >
-                          {item}
-                        </Link>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </>
+           <LanguageClient currLanguage={currLanguage} params={params} />
           )}
         </div>
       </div>
